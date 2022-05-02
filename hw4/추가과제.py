9026edd0fcdd105256b4bf97c7c212ab64d60070
천승범 <0921sean@yonsei.ac.kr>
2022-05-02 15:44:39 +0900

    추가과제

---
+++ hw4/추가과제.py
@@ -0,0 +1,17 @@
+# HW 1
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+print("몫:  " + str(num1 // num2))
+print("나머지:  " + str(num1 % num2))
+
+# HW 2
+x = int(input("Enter a value for x: "))
+y = 3 * x**5 + 2 * x**4 - 5 * x**3 - x**2 + 7 * x - 6
+print("Polynomial for x= 8 :  " + str(y))
+
+# HW 3
+n = int(input("Enter the number of people : "))
+prob = 100
+for i in range(n):
+    prob *= (1-i/365)
+print("Probability: " + str(100-prob))
